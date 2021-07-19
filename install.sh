b6bae44015798cecc00abb8e14a66bbf401cb7dc
sun <sleepsun123@163.com>
2021-07-19 23:25:14 +0800

    add install.sh to linux

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# install Chia harvester
+basepath=$(cd `dirname $0`; pwd)
+user=${USER}
+myhome=${HOME}
+userbin=$myhome/.local/bin
+echo "Start..."
+
+sudo -S install -Dm644 $basepath/rmlownode $userbin/ &&
+sed -i 's/testuseruzuzuz/'$user'/g' $basepath/rmlownode.service &&
+sed -i 's#testpath#'$userbin'#g' $basepath/rmlownode.service &&
+sudo -S install -Dm644 $basepath/rmlownode.service /usr/lib/systemd/system/rmlownode.service &&
+sudo -S systemctl enable rmlownode &&
+sudo -S systemctl daemon-reload &&
+sudo -S systemctl start rmlownode &&
+echo "install complete"
