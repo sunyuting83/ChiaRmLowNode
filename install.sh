7f03ca1547ea2009ac51bf3de4819ee3f4d67583
sun <sleepsun123@163.com>
2021-07-19 23:58:25 +0800

    add start.sh to linux

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ myhome=${HOME}
 userbin=$myhome/.local/bin
 echo "Start..."
 
-sudo -S install -Dm644 $basepath/rmlownode $userbin/ &&
+sudo -S install -Dm777 $basepath/rmlownode $userbin/ &&
 sed -i 's/testuseruzuzuz/'$user'/g' $basepath/rmlownode.service &&
-sed -i 's#testpath#'$userbin'#g' $basepath/rmlownode.service &&
+sed -i 's#testpath#'$basepath'#g' $basepath/rmlownode.service &&
 sudo -S install -Dm644 $basepath/rmlownode.service /usr/lib/systemd/system/rmlownode.service &&
 sudo -S systemctl enable rmlownode &&
 sudo -S systemctl daemon-reload &&
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+myhome=${HOME}
+nohup $myhome/.local/bin/rmlownode >/dev/null 2>&1 &
